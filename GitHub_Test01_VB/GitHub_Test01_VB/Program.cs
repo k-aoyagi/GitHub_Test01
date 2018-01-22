c7c83524702797a4240b092adb3436e6dc1469bd
k-aoyagi <k-aoyagi@its-corp.co.jp>
2018-01-22 14:18:07 +0900

    commit test01_VB

---
+++ GitHub_Test01_VB/GitHub_Test01_VB/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Test01_VB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rem test01_VB
+        }
+    }
+}
